bc7fa9478dda2012c8678f9d322ad0a0da135b38
long ngo <ngothanhlongnoyssiw@gmail.com>
2021-04-07 09:19:47 +0700

    create helpers

--- src/index.js
+++ src/index.js
@@ -33,28 +33,7 @@ app.engine(
     'hbs',
     exphbs({
         extname: '.hbs',
-        helpers: {
-            sum: (a, b) => a + b,
-            sortTable: (col, sort) => {
-                const types = {
-                    default: 'desc',
-                    desc: 'asc',
-                    asc: 'desc'
-                }
-
-                const icons = {
-                    default: 'fa-sort',
-                    asc: 'fa-sort-amount-up',
-                    desc: 'fa-sort-amount-down'
-                }
-
-                let type = (col === sort.collumn) ? sort.type : 'default';
-
-                return `<a href="?_sort&collumn=${col}&type=${types[type]}">
-                    <i class="fas ${icons[type]}"></i>
-                    </a>`;
-            }
-        }
+        helpers: require('./helpers/handlebars')
     })
 );
 app.set('view engine', 'hbs');
